8a01d8c0619f648e784b5aa08981eafd522d2a30
Dmitry Alenichev <mitya@rockers.su>
2015-09-07 21:24:19 +0300

    separate upstream_addr with "/"

--- log.lua
+++ log.lua
@@ -2,6 +2,7 @@ local stats = ngx.shared.stats
 local zone = ngx.var.zone or "default"
 
 function key(name) return name .. ":" .. zone end
+function akey(name, addr) return key(name) .. "/" .. addr end
 
 local status = "status_" .. ngx.var.status
 
@@ -53,26 +54,26 @@ if upstream_addr then
     local up_status = ngx.var.upstream_status:gmatch("(%d+),? ?:?")
 
     for addr in string.gmatch(upstream_addr, "([0-9a-zA-Z%.:/]+),? ?:?") do
-        local connect_time = key("upstream_connect_time_" .. addr)
+        local connect_time = akey("upstream_connect_time", addr)
         local c_time = stats:get(connect_time) or 0
         c_time = c_time + conn_time()
         stats:set(connect_time, c_time)
 
-        local header_time = key("upstream_header_time_" .. addr)
+        local header_time = akey("upstream_header_time", addr)
         local h_time = stats:get(header_time) or 0
         h_time = h_time + head_time()
         stats:set(header_time, h_time)
 
-        local response_time = key("upstream_response_time_" .. addr)
+        local response_time = akey("upstream_response_time", addr)
         local r_time = stats:get(response_time) or 0
         r_time = r_time + resp_time()
         stats:set(response_time, r_time)
 
-        local upstream_key = "upstream_status_" .. up_status() .. "_" .. addr
-        local newval, err = stats:incr(key(upstream_key), 1)
+        local upstream_key = akey("upstream_status_" .. up_status(), addr)
+        local newval, err = stats:incr(upstream_key, 1)
         if not newval and err == "not found" then
-            stats:add(key(upstream_key), 0)
-            stats:incr(key(upstream_key), 1)
+            stats:add(upstream_key, 0)
+            stats:incr(upstream_key, 1)
         end
     end
 end
